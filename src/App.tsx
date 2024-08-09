41305b7fe445fe7689789592eae31727697dfad6
Max <Maxghonson77@gmail.com>
2024-08-09 07:05:50 +0500

    new food project

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ const App = () => {
     let [foods, setFoods] = useState<FoodsProps[]>([]);
     let [selectFood, setSelectFood] = useState<FoodsProps>();
     let [hide, setHide] = useState(true);
-    let [searchTitle, setSearchTitle] = useState("");
+    let [qidiruv, setQidiruv] = useState("");
 
     useEffect(() => {
         axios
-            .get("https://www.themealdb.com/api/json/v1/1/search.php?s=beef")
+            .get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
             .then((data) => {
                 setFoods(data.data.meals);
                 setSelectFood(data.data.meals[0]);
@@ -19,18 +19,18 @@ const App = () => {
             .catch((err) => console.log(err.message));
     }, []);
 
-    const handleNextClick = (item: FoodsProps) => {
+    const Next = (item: FoodsProps) => {
         setSelectFood(item);
         setHide(false);
     };
 
-    const handleHideClick = () => {
+    const Hide = () => {
         setHide(true);
     };
 
-    const handleSearch = () => {
+    const Search = () => {
         axios
-            .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTitle}`)
+            .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${qidiruv}`)
             .then((data) => {
                 setFoods(data.data.meals);
                 setSelectFood(data.data.meals[0]);
@@ -40,7 +40,7 @@ const App = () => {
 
     const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-            handleSearch();
+            Search();
             e.currentTarget.blur(); 
         }
     };
@@ -54,8 +54,8 @@ const App = () => {
                         type="search"
                         className="bg-transparent border-2 p-2 rounded-sm placeholder:text-white text-white border-white"
                         placeholder="Titleni kiriting"
-                        value={searchTitle}
-                        onChange={(e) => setSearchTitle(e.target.value)}
+                        value={qidiruv}
+                        onChange={(e) => setQidiruv(e.target.value)}
                         onKeyPress={handleKeyPress}
                     />
                     <input
@@ -68,19 +68,19 @@ const App = () => {
             {hide ? (
                 <div className="w-11/12 my-0 py-5 mx-auto grid grid-cols-3 gap-2">
                     {foods?.map((item, index) => (
-                        <div key={index} onClick={() => handleNextClick(item)} className="next shadow-md cursor-pointer duration-300 hover:shadow-slate-800 rounded-md shadow-slate-500 p-5">
+                        <div key={index} onClick={() => Next(item)} className="next shadow-md cursor-pointer duration-300 hover:shadow-slate-800 rounded-md shadow-slate-500 p-5">
                             <img src={item.strMealThumb} alt={item.strMeal} />
                             <h3 className="text-xl mt-2">
-                                <b>Sarlavha: </b>
+                                <b>Title: </b>
                                 {item.strMeal}
                             </h3>
                             <div className="my-2 flex justify-between">
                                 <p>
-                                    <b>Kategoriya: </b>
+                                    <b>Category: </b>
                                     {item.strCategory}
                                 </p>
                                 <p>
-                                    <b>Hudud: </b>
+                                    <b>Location: </b>
                                     {item.strArea}
                                 </p>
                             </div>
@@ -89,15 +89,15 @@ const App = () => {
                 </div>
             ) : (
                 <div className="flex justify-center items-center w-[100vw] h-[110vh] bg-slate-200">
-                    <div className="first shadow-md shadow-orange-700 p-5 mt-16 rounded-md w-[60vw] h-[110vh]">
-                        <button onClick={handleHideClick} className="absolute top-[100px] right-[180px] bg-red-500 text-white px-2 py-1 rounded">X</button>
+                    <div className="first shadow-md shadow-orange-700 p-5 mt-16 rounded-md w-[60vw] h-[90vh]">
+                        <button onClick={Hide} className="absolute top-[100px] right-[180px] bg-red-500 text-white px-2 py-1 rounded">X</button>
                         <h4>{selectFood?.strMeal}</h4>
                         <p>{selectFood?.strInstructions}</p>
                         <iframe
                             className="ml-16"
                             width="640"
                             height="290"
-                            src={`https://www.youtube.com/embed/${selectFood?.strYoutube.slice(31)}`}
+                            src={`https://www.youtube.com/embed/${selectFood?.strYoutube.slice(32)}`}
                             title={selectFood?.strMeal}
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             allowFullScreen
